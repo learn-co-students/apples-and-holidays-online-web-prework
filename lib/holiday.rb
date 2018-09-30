c0dc97e480735d6bde7fb9be06a1f98d507c3212
Bryce Eadie <bryce.eadie@gmail.com>
2018-09-30 19:12:01 +0000

    Done.

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -31,9 +31,8 @@ def add_supply_to_memorial_day(holiday_hash, supply)
 end
 
 def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)
-  # code here
-  # remember to return the updated hash
-
+  holiday_hash[season][holiday_name] = supply_array
+  holiday_hash
 end
 
 def all_winter_holiday_supplies(holiday_hash)
@@ -47,20 +46,31 @@ def all_winter_holiday_supplies(holiday_hash)
 end
 
 def all_supplies_in_holidays(holiday_hash)
-  # iterate through holiday_hash and print items such that your readout resembles:
-  # Winter:
-  #   Christmas: Lights, Wreath
-  #   New Years: Party Hats
-  # Summer:
-  #   Fourth Of July: Fireworks, BBQ
-  # etc.
-
+  holiday_hash.each do |season, holiday|
+    puts "#{season.to_s.capitalize}:"
+      holiday.each do |name, supply|
+        final_events = []
+        events = name.to_s.split('_')
+          events.each do |element|
+            final_events << element.capitalize
+          end
+        puts "  #{final_events.join(" ")}: #{supply.join(", ")}"
+      end
+  end
 end
 
 def all_holidays_with_bbq(holiday_hash)
-  # return an array of holiday names (as symbols) where supply lists
-  # include the string "BBQ"
-
+  array = []
+  holiday_hash.each do |season, holiday|
+    holiday.each do |event, supply|
+      supply.each do |item|
+        if item == "BBQ"
+        array << holiday.keys
+        end
+      end
+    end
+  end
+  array.flatten
 end
 
 
