a4a77eca36573f8988c80190b53026f4b14c7e2b
Brandon Teich <brandonteich123@gmail.com>
2018-10-14 21:46:33 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -59,7 +59,7 @@ def all_supplies_in_holidays(holiday_hash)
   #   Fourth Of July: Fireworks, BBQ
   # etc.
   holiday_hash.each do |key, value|
-  puts "#{key.capitalize}:"
+    puts "#{key.capitalize}:"
     value.each do |inner_key, inner_value|
       inner_key = inner_key.to_s.split("_")
       inner_key = inner_key.map {|word| word.capitalize }.join(' ')
