87080e146432c62e7c26e2297d6439fb6679afd0
A Glazer <avigayilglazer@gmail.com>
2019-01-12 19:09:28 +0000

    Done.

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -93,9 +93,16 @@ holiday_supplies.each do |season, holiday|
 
 end
 
+#need help
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
 
-  holiday_supplies.keys
+  holiday_supplies.each do |season, holiday|
+    holiday.each do |holiday, item|
+      if item == "BBQ"
+        puts holiday
+      end
+    end
+  end
 end
