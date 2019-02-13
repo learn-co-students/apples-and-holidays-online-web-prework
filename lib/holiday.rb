323173ef1462ea9c16e7f355945bd1d55d043196
Audrey Jones <audrey.eliz.jones@gmail.com>
2019-02-13 18:21:18 +0000

    Done.

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -58,18 +58,11 @@ def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each {|season, holiday|
     puts "#{season.to_s.capitalize}:"
     holiday.each {|holiday,supplies|
-      puts "  #{holiday.to_s.capitalize}: #{supplies.join(", ")}"
-      binding.pry
+      puts "  #{holiday.to_s.split("_").map(&:capitalize).join(' ')}: #{supplies.join(", ")}"
       }
     }
-
-  # Winter:
-  #   Christmas: Lights, Wreath
-  #   New Years: Party Hats
-  # Summer:
-  #   Fourth Of July: Fireworks, BBQ
-  # etc.
 end
+
 
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
